7ba727df3c5b9a26366892c93b6cb505c08c3235
shen <shenxuhui1992@163.com>
2017-03-15 19:44:54 +0800

    fix spacemacs tab indent bug

--- example.c
+++ example.c
@@ -3,9 +3,9 @@
 
 unit_test(test_example)
 {
-				int a = 1;
-				printf("test a = %d\n", a);
-				test_assert(a == 1);
+	int a = 1;
+	printf("test a = %d\n", a);
+	test_assert(a == 1);
 }
 
 test_file_end
